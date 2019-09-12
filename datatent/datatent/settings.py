5ea4764169f9f587978948cebb1bce0c58bb11f3
Tarik van den Berg <tarikvdb@gmail.com>
2019-09-12 16:12:05 +0200

    Added database models in datatent_api

--- datatent/datatent/settings.py
+++ datatent/datatent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5+kv4m$24edyvjp*eg)iv$hr$)#=!@k9m41(fu9r9wh1#zjgwp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'datatent_frontend',
+    'datatent_api',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ datatent/datatent_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DatatentApiConfig(AppConfig):
+    name = 'datatent_api'
