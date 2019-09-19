4d6a2c90b7dd2a5c6ade3b0bdddf35e76e24e051
Yoran Vulker <yvulker@gmail.com>
2019-09-19 14:29:27 +0200

    Remove disconnected clients from clients dictionary

--- messinq-backend/index.js
+++ messinq-backend/index.js
@@ -20,12 +20,23 @@ var onMessage = (websocket, request) => (message) => {
         switch (data.event) {
             case "client_register":
                 clients[data.data.username] = websocket;
+
                 websocket.send(JSON.stringify({
                     event: "client_register",
                     data: {
                         message: `Registered as ${data.data.username}`,
                     },
                 }));
+
+                for (var client in clients) {
+                    try {
+                        sendClientList(clients[client]);
+                    } catch (ex) {
+                        console.warn(`Exception while sending updated client list to '${client}', removing him from the client list.`);
+                        delete clients[client];
+                    }
+                }
+
                 break;
             case "message":
                 clients[data.data.recipient].send(JSON.stringify({
@@ -59,6 +70,16 @@ app.use(express.static('public'));
 // register web socket on /, will fire on new websocket connections
 app.ws('/', (websocket, request) => {
     websocket.on("message", onMessage(websocket, request));
+    websocket.on("close", () => {
+        for (var client in clients) {
+            if (clients[client] === websocket) {
+                delete clients[client];
+
+                console.info(`Removed client '${client}'`);
+                return;
+            }
+        }
+    });
 })
 
 app.listen(3000, () => {
