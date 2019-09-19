3da77dcacbc4f6c08d26d6d31874aefc1bf1426a
Yoran Vulker <yvulker@gmail.com>
2019-09-19 14:32:09 +0200

    Add functionality to temp frontend (user list, messaging)

--- messinq-backend/public/script.js
+++ messinq-backend/public/script.js
@@ -1,8 +1,68 @@
 window.onload = (event) => {
     var username = prompt("Please enter your username to continue");
-    var socket = new WebSocket("ws://localhost:3000/");
+    var socket = new WebSocket(`ws://${location.host}/`);
 
-    var clientList = document.querySelector("#clients");
+    var clientsList = document.querySelector("#clients");
+    var dialog = document.querySelector("#messages_container");
+    var messageForm = document.querySelector("#message_form");
+    var messageText = document.querySelector("#message_text");
+    var messages = {};
+
+    var updateMessageWindow = () => {
+        const selectedUser = clientsList.options[clientsList.selectedIndex].value;
+
+        if (messages.hasOwnProperty(selectedUser)) {
+            var dialogText = `<p>Showing messages with ${selectedUser}</p>\n`;
+
+            messages[selectedUser].forEach(({ from, message }) => {
+                dialogText += `<p><strong>${from}: </strong>${message}</p>\n`;
+            });
+
+            dialog.innerHTML = dialogText;
+        } else {
+            dialog.innerHTML = `<p>No messages with user ${selectedUser} yet.</p>`;
+        }
+    };
+
+    clientsList.onchange = (event) => updateMessageWindow();
+
+    messageForm.onsubmit = (event) => {
+        event.preventDefault();
+
+        const recipient = clientsList.options[clientsList.selectedIndex].value;
+
+        if (recipient === "") {
+            alert("Please select the user you want to send your message to.");
+            return false;
+        }
+
+        socket.send(JSON.stringify({
+            event: "message",
+            data: {
+                sender: username,
+                recipient: clientsList.options[clientsList.selectedIndex].value,
+                message:  messageText.value,
+            },
+        }));
+
+        if (messages.hasOwnProperty(recipient)) {
+            messages[recipient].push({
+                from: username,
+                to: recipient,
+                message: messageText.value,
+            });
+        } else {
+            messages[recipient] = [{
+                from: username,
+                to: recipient,
+                message: messageText.value,
+            }];
+        }
+
+        updateMessageWindow();
+
+        return false;
+    };
 
     socket.onopen = (event) => {
         socket.send(JSON.stringify({
@@ -26,10 +86,32 @@ window.onload = (event) => {
                     }));
                     break;
                 case "message":
-                    console.log(`From '${data.data.from}': ${data.data.message}`);
+                    const { from, to, message } = data.data;
+
+                    if (messages.hasOwnProperty(from)) {
+                        messages[from].push({
+                            from,
+                            to,
+                            message,
+                        });
+                    } else {
+                        messages[from] = [{
+                            from,
+                            to,
+                            message,
+                        }];
+                    }
+
+                    updateMessageWindow();
+
                     break;
                 case "client_list":
-                    console.log("Client list", data.data.clients);
+                    var clients = data.data.clients;
+                    var clientsOptions = "<option value=''>Select user</option>";
+                    clients.forEach(client => {
+                        clientsOptions += `<option value='${client}'>${client}</option>`;
+                    });
+                    clientsList.innerHTML = clientsOptions;
                     break;
                 default:
                     console.error(`Unknown event ${data.event}`);
@@ -44,5 +126,4 @@ window.onload = (event) => {
     socket.onclose = (event) => {
 
     };
-
 };
