72ac294ff28dacc39db42b0ae4b056e6c1e02c4b
- <jschooldropbox@email.cz>
2019-10-01 14:48:36 +0200

    Converted database to UTF8mb4_unicode_ci

--- database/snetech.sql
+++ database/snetech.sql
@@ -6,8 +6,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE IF NOT EXISTS `school_snetech` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `school_snetech`;
+/*CREATE DATABASE IF NOT EXISTS `school_snetech` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;*/
+USE `messinq`;
 
 CREATE TABLE IF NOT EXISTS `bericht` (
   `bericht_id` int(255) NOT NULL AUTO_INCREMENT,
@@ -16,17 +16,17 @@ CREATE TABLE IF NOT EXISTS `bericht` (
   `gesprek_id` int(255) NOT NULL,
   `tijd_verstuurd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`bericht_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `gebruiker` (
   `gebruiker_id` int(255) NOT NULL,
   `gebruikers_naam` int(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `profielfoto` varchar(255) NOT NULL,
-  `emailadres` varchar(255) NOT NULL,
-  `wachtwoordhash` varchar(255) NOT NULL,
+  `status` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `profielfoto` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `emailadres` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `wachtwoordhash` varchar(255) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`gebruiker_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `gesprek` (
   `gesprek_id` int(255) NOT NULL,
@@ -34,19 +34,19 @@ CREATE TABLE IF NOT EXISTS `gesprek` (
   `groeps_id` int(255) NOT NULL,
   `gelezen` int(1) NOT NULL,
   PRIMARY KEY (`gesprek_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `groep` (
   `groeps_id` int(255) NOT NULL,
-  `groeps_naam` varchar(255) NOT NULL,
+  `groeps_naam` varchar(255) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`groeps_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `key` (
   `gebruiker_id` int(255) NOT NULL,
   `public_key` int(255) NOT NULL,
   `private_key` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
